4fd05fcdefe06f67cfd4c16ec48bc099d51e7cc3
dubisdev <dubisdev@gmail.com>
2025-05-27 21:32:28 +0200

    chore: export & import services and modules

--- car-dealership/src/cars/cars.service.ts
+++ car-dealership/src/cars/cars.service.ts
@@ -10,11 +10,7 @@ import { UpdateCarDto } from './dto/update-car.dto';
 
 @Injectable()
 export class CarsService {
-  private cars: Car[] = [
-    // { id: uuid(), brand: 'Toyota', model: 'Corolla' },
-    // { id: uuid(), brand: 'Honda', model: 'Civic' },
-    // { id: uuid(), brand: 'Jeep', model: 'Cherokee' },
-  ];
+  private cars: Car[] = [];
 
   findAll() {
     return this.cars;
--- car-dealership/src/seed/data/brands.seed.ts
+++ car-dealership/src/seed/data/brands.seed.ts
@@ -1,7 +1,7 @@
 import { Brand } from '@/brands/entities/brand.entity';
 import { v4 as uuid } from 'uuid';
 
-export const CARS_SEED: Brand[] = [
+export const BRANDS_SEED: Brand[] = [
   {
     id: uuid(),
     name: 'Volvo',

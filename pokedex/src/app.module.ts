c9f0f14c45d10db16c388dacb90c403cad0a477f
dubisdev <dubisdev@gmail.com>
2025-05-28 18:03:47 +0200

    chore: configure versioning

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { resolve } from 'path';
+import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: resolve('public'),
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api/{*test}'], // Exclude API routes from static serving
+      serveStaticOptions: {
+        fallthrough: false, // Prevent serving static files for non-existent routes
+      },
     }),
     PokemonModule,
   ],

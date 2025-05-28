d421f2d2697f7dd7d9e788e760f74c69d73070dd
dubisdev <dubisdev@gmail.com>
2025-05-28 17:49:50 +0200

    chore: Add base pokemon resource

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: 'public',
+      rootPath: resolve('public'),
     }),
+    PokemonModule,
   ],
 })
 export class AppModule {}

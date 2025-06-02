692a0cb224dfe530327f1ec7783da5c558213dc5
dubisdev <dubisdev@gmail.com>
2025-06-02 19:12:24 +0200

    chore: Add custom validation pipe + improve delete workflow

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -3,6 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -17,6 +18,8 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
     PokemonModule,
+
+    CommonModule,
   ],
 })
 export class AppModule {}

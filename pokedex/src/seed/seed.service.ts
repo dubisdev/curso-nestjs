9eef0c5608be46ef737c1e34ab33474bab75fc3e
dubisdev <dubisdev@gmail.com>
2025-06-02 19:44:52 +0200

    chore: get pokemons from pokepi

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PokeReponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed successfully';
+  async executeSeed() {
+    const data = (await fetch(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    ).then((res) => res.json())) as PokeReponse;
+
+    const apiData = data.results.map(({ name, url }) => {
+      const segments = url.split('/');
+      const id = segments.at(-2);
+
+      if (!id) {
+        throw new Error(`ID not found in URL: ${url}`);
+      }
+
+      return { name, id: +id };
+    });
+
+    return apiData;
   }
 }

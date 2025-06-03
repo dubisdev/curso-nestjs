36847ee5c22fbd1fe4882797fb0019a5619703a6
dubisdev <dubisdev@gmail.com>
2025-06-03 20:08:35 +0200

    chore: Add env config

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -5,9 +5,17 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration], // First we map the env variables
+      validationSchema: JoiValidationSchema, // Then we validate them
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       exclude: ['/api/{*test}'], // Exclude API routes from static serving

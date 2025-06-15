9a593d174fde67e3ba205b461afd3442b6768722
dubisdev <dubisdev@gmail.com>
2025-06-15 13:45:52 +0200

    chore: Setup JwtModule

--- teslo-shop/src/auth/auth.module.ts
+++ teslo-shop/src/auth/auth.module.ts
@@ -3,11 +3,31 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow<string>('JWT_EXPIRATION'),
+        },
+      }),
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}

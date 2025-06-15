455400075303a72136010296037c9412321e8d37
dubisdev <dubisdev@gmail.com>
2025-06-15 11:48:21 +0200

    chore: Add user password hassing

--- teslo-shop/src/auth/auth.service.ts
+++ teslo-shop/src/auth/auth.service.ts
@@ -5,8 +5,9 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,9 +18,17 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
 
       await this.userRepository.save(user);
+
+      // @ts-expect-error Password should not be deleted as it is not optional in the User entity
+      delete user.password; // Remove password from the response
 
       return user;
     } catch (error) {
@@ -28,8 +37,12 @@ export class AuthService {
   }
 
   private handleDbError(error: any): never {
-    if (error.code === '23505') {
-      throw new BadRequestException(error.detail);
+    if (error instanceof QueryFailedError) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if ((error as any).code === '23505') {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        throw new BadRequestException((error as any).detail);
+      }
     }
 
     console.log(error);

ac2cb2e337b2c7253dda5085a79710bd8bfec6d5
dubisdev <dubisdev@gmail.com>
2025-06-16 20:34:50 +0200

    chore: Add documentation for dtos

--- teslo-shop/src/common/dtos/pagination.dto.ts
+++ teslo-shop/src/common/dtos/pagination.dto.ts
@@ -1,12 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'Number of items to return per page',
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description:
+      'Number of items to skip before starting to collect the result set',
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
--- teslo-shop/src/products/entities/product.entity.ts
+++ teslo-shop/src/products/entities/product.entity.ts
@@ -9,44 +9,69 @@ import {
 } from 'typeorm';
 import { ProductImage } from './product-image.entity';
 import { User } from '../../auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: '7e1c9913-bed1-4d0f-a65d-64f67fea4ef0',
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'Teslo Shirt',
+    description: 'The title of the product',
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: 12.55,
+    description: 'The price of the product',
+  })
   @Column('float', {
     default: 0,
   })
   price: number;
 
+  @ApiProperty({
+    example: 'A comfortable and stylish shirt for everyday wear.',
+    description: 'A brief description of the product',
+  })
   @Column({
     type: 'text',
     nullable: true,
   })
   description: string;
 
+  @ApiProperty({
+    example: 'teslo-shirt',
+    description: 'A unique slug for the product, used in URLs',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   slug: string;
 
+  @ApiProperty()
   @Column('int', { default: 0 })
   stock: number;
 
+  @ApiProperty()
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
+  @ApiProperty()
   @Column('text')
   gender: string;
 
+  @ApiProperty()
   @Column({
     type: 'text',
     array: true,
@@ -54,6 +79,7 @@ export class Product {
   })
   tags: string[];
 
+  @ApiProperty()
   @OneToMany(() => ProductImage, (image) => image.product, {
     cascade: true,
     eager: true,

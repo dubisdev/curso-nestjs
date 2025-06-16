df64919987ab21b20df42c49b48b59d8e112052f
dubisdev <dubisdev@gmail.com>
2025-06-16 19:09:44 +0200

    chore: custom decorators

--- teslo-shop/src/auth/auth.controller.ts
+++ teslo-shop/src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser, RawHeaders } from './decorators';
+import { Auth, GetUser, RawHeaders } from './decorators';
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -22,6 +25,35 @@ export class AuthController {
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
+    @GetUser(['email', 'fullName']) user: User,
+    @RawHeaders() rawHeaders: string[],
+  ) {
+    console.log(user);
+    return {
+      rawHeaders,
+      ok: true,
+      message: 'You are authenticated',
+    };
+  }
+
+  @Get('private2')
+  @RoleProtected(ValidRoles.superUser)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  testingPrivateRoute2(
+    @GetUser(['email', 'fullName']) user: User,
+    @RawHeaders() rawHeaders: string[],
+  ) {
+    console.log(user);
+    return {
+      rawHeaders,
+      ok: true,
+      message: 'You are authenticated',
+    };
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.superUser)
+  testingPrivateRoute3(
     @GetUser(['email', 'fullName']) user: User,
     @RawHeaders() rawHeaders: string[],
   ) {
---
+++ teslo-shop/src/auth/guards/user-role.guard.ts
@@ -0,0 +1,46 @@
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { User } from '../entities/user.entity';
+import { Request } from 'express';
+import { META_ROLES } from '../decorators/role-protected.decorator';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const validRoles: string[] = this.reflector.get<string[]>(
+      META_ROLES,
+      context.getHandler(),
+    );
+
+    const req = context.switchToHttp().getRequest<Request>();
+
+    const user = req.user as User;
+
+    if (!user) {
+      throw new BadRequestException('User not found in request context');
+    }
+
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) {
+        return true;
+      }
+    }
+    throw new ForbiddenException(
+      `User ${user.fullName} does not have the required roles: ${validRoles.join(', ')}`,
+    );
+  }
+}

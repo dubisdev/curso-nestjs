ece1668fafdd73d28dba6537e85de832054e1db3
dubisdev <dubisdev@gmail.com>
2025-06-17 18:52:38 +0200

    chore: keep track of ws server status on client

--- websocket-client/src/socket-client.ts
+++ websocket-client/src/socket-client.ts
@@ -1,7 +1,23 @@
-import { Manager } from "socket.io-client"
+import { Manager, Socket } from "socket.io-client"
 
 export const connectToServer = () => {
     const manager = new Manager("http://localhost:3000/socket.io/socket.io")
 
     const socket = manager.socket("/")
+
+    addListeners(socket)
+}
+
+
+const addListeners = (socket: Socket) => {
+    const $serverStatusLablel = document.querySelector<HTMLSpanElement>("#server-status")!
+
+    socket.on("connect", () => {
+        $serverStatusLablel.innerHTML = "Connected"
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected from server")
+         $serverStatusLablel.innerHTML = "Disconnected"
+    })
 }
